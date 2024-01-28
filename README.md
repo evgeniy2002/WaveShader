befc72068c6c8606131a6999b719bb10c4a8de66
Evgeniy Kobzev <evgenij.kobzev@index.com>
2024-01-29 00:44:10 +0300

    glsl: first commit

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# r3f-vite-starter
+# r3f-vite-project
 A boilerplate to build R3F projects
 
 ```
@@ -6,5 +6,3 @@ yarn
 yarn dev
 ```
 
-
-![image](https://user-images.githubusercontent.com/6551176/221732091-23ee52cb-4150-42fa-b998-43628d7a6b0d.png)
--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,27 @@
 import { Canvas } from "@react-three/fiber";
 import { Experience } from "./components/Experience";
+import { Suspense } from "react";
+
+
+function Wave(){
+  return (
+    <Canvas camera={{fov: 8}}>
+      <color attach="background" args={["#181717"]} />
+      
+      <Suspense fallback={null}>
+        <Experience />
+      </Suspense>
+    </Canvas>
+  )
+}
 
 function App() {
   return (
-    <Canvas shadows camera={{ position: [3, 3, 3], fov: 30 }}>
-      <color attach="background" args={["#ececec"]} />
-      <Experience />
-    </Canvas>
-  );
+    <>
+      <h1>Oceanic Canvas</h1>
+      <Wave />
+    </>  
+  )
 }
 
 export default App;
